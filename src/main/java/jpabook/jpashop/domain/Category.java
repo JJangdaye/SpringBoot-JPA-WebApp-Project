72f7d8071e20f5d287c0a4869acb1978216eedf7
daye <moda1019@naver.com>
2025-04-02 08:19:43 +0900

    Change the FetchType of @XToOne from EAGER to LAZY

--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -8,6 +8,8 @@ import lombok.Setter;
 import java.util.ArrayList;
 import java.util.List;
 
+import static jakarta.persistence.FetchType.*;
+
 @Entity
 @Getter
 @Setter
@@ -28,7 +30,7 @@ public class Category {
     private List<Item> items = new ArrayList<>();
 
     // 같은 엔티티에 대해서 셀프로 양방향 연관관계를 건 것임
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name="parent_id")
     private Category parent;
 
